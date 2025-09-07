0ad5e3e174d0c61f07ac1b0ba792da0f9c65cf0b
Paoo <thienhanaylai@gmail.com>
2025-09-07 15:00:11 +0700

    first commit

---
+++ vancy-shop/src/components/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <>
+      <div>
+        <div></div>
+        <div>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
